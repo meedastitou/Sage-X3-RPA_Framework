d3e9f63cb80d68e7873f09cce6da1c34819edc83
meedastitou <astitoumd@gmail.com>
2026-01-07 19:32:33 +0100

    add disconnect from sage

--- modules/bonne_commande/bonne_commande_robot.py
+++ modules/bonne_commande/bonne_commande_robot.py
@@ -38,9 +38,9 @@ class BonneCommandeRobot(BaseRobot, WebResultMixin):
         self.driver_manager.headless = headless
 
         # URLs des modules
-        self.url_article = "http://192.168.1.241:8124/syracuse-main/html/main.html?url=%2Ftrans%2Fx3%2Ferp%2FBASE1%2F%24sessions%3Ff%3DGESITM%252F2%252F%252FM%252F%26profile%3D~(loc~%27fr-FR~role~%278ecdb3d1-8ca7-40ca-af08-76cb58c70740~ep~%27cb006c17-58a5-4b98-9f2b-474ec03472a3~appConn~())"
-        self.url_demande_achat = "http://192.168.1.241:8124/syracuse-main/html/main.html?url=%2Ftrans%2Fx3%2Ferp%2FBASE1%2F%24sessions%3Ff%3DGESPSH%252F2%252F%252FM%252F%26profile%3D~(loc~%27fr-FR~role~%278ecdb3d1-8ca7-40ca-af08-76cb58c70740~ep~%27cb006c17-58a5-4b98-9f2b-474ec03472a3~appConn~())"
-        self.url_bonne_commande = "http://192.168.1.241:8124/syracuse-main/html/main.html?url=%2Ftrans%2Fx3%2Ferp%2FBASE1%2F%24sessions%3Ff%3DXBCAUTO%252F2%252F%252FM%252F"
+        self.url_article = "http://192.168.1.252:8124/syracuse-main/html/main.html?url=%2Ftrans%2Fx3%2Ferp%2FBASE1%2F%24sessions%3Ff%3DGESITM%252F2%252F%252FM%252F%26profile%3D~(loc~%27fr-FR~role~%278ecdb3d1-8ca7-40ca-af08-76cb58c70740~ep~%27cb006c17-58a5-4b98-9f2b-474ec03472a3~appConn~())"
+        self.url_demande_achat = "http://192.168.1.252:8124/syracuse-main/html/main.html?url=%2Ftrans%2Fx3%2Ferp%2FBASE1%2F%24sessions%3Ff%3DGESPSH%252F2%252F%252FM%252F%26profile%3D~(loc~%27fr-FR~role~%278ecdb3d1-8ca7-40ca-af08-76cb58c70740~ep~%27cb006c17-58a5-4b98-9f2b-474ec03472a3~appConn~())"
+        self.url_bonne_commande = "http://192.168.1.252:8124/syracuse-main/html/main.html?url=%2Ftrans%2Fx3%2Ferp%2FBASE1%2F%24sessions%3Ff%3DXBCAUTO%252F2%252F%252FM%252F"
 
         # Compteurs pour validation stricte
         self.articles_traites = 0
@@ -235,6 +235,10 @@ class BonneCommandeRobot(BaseRobot, WebResultMixin):
             # ✨ ENVOYER LES RÉSULTATS (même en cas d'erreur)
             self.logger.info("✨ Envoi des résultats vers l'endpoint web malgré l'erreur critique...")
             self.send_results_to_web(email_achteur)
+        
+        finally:
+            self.logger.info("Deconnexion du robot...")
+            self.disconnect_sage()
     
     def _lire_et_valider_excel(self, excel_file: str) -> pd.DataFrame:
         """Lire et valider le fichier Excel"""
